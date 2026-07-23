35b8c0cb76b4f31c8b42eb12370962f8efb5d1a1
Erlangga <erlangga.developer@gmail.com>
2026-07-24 00:26:18 +0700

    Add initial user accounts to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::updateOrCreate(
+            ['name' => 'QC User'],
+            [
+                'password' => Hash::make('password123'),
+                'role' => 'qc',
+                'status' => 'aktif',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['name' => 'Penimbang User'],
+            [
+                'password' => Hash::make('password123'),
+                'role' => 'penimbang',
+                'status' => 'aktif',
+            ]
+        );
+
+        User::updateOrCreate(
+            ['name' => 'Kasir User'],
+            [
+                'password' => Hash::make('password123'),
+                'role' => 'kasir',
+                'status' => 'aktif',
+            ]
+        );
     }
 }
